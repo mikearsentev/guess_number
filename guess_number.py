2a423de393021068d853995eacf2f4739d582fdf
Mike Arsentev <mikearsentev@gmail.com>
2025-07-24 10:11:22 +0300

    add initial commit

--- guess_number.py
+++ guess_number.py
@@ -5,6 +5,7 @@ from random import randint
 # Получаем случайное число в диапазоне от 1 до 100.
 number = randint(1, 100)
 print('Угадайте число от 1 до 100')
+print("test changes")
 
 while True:
     # Получаем число от пользователя и сохраняем его в переменную.
